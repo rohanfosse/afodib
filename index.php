f635e50f0d85604c0ebaab9ad124199b8de8264d
rohan <rohan.fosse@labri.fr>
2018-03-06 23:03:11 +0100

    bug fixes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-	<!DOCTYPE html>
+    <!DOCTYPE html>
 <html>
 <title>Café Michel - AFoDIB</title>
 <meta charset="UTF-8">
@@ -37,7 +37,8 @@ width:400px;
 height:300px;
 }
 </style>
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
 <script type="text/javascript">
     $(function($){
         var addToAll = false;
@@ -65,34 +66,37 @@ height:300px;
 $(document).ready(function(){
     $("button").click(function(){
 $.ajax({
-	  type: "GET",  
-	  url: "tarifs.csv",
-	  dataType: "text",       
-	  success: function(response)  
-	  {
-		data = $.csv.toArrays(response);
-		var ref = parseInt(document.getElementById("id").value);
-		data_line = find_line(data,ref);
-		ajouter(data_line);
-	  }   
-	});
+      type: "GET",  
+      url: "tarifs.csv",
+      dataType: "text",       
+      success: function(response)  
+      {
+        data = $.csv.toArrays(response);
+        var ref = parseInt(document.getElementById("id").value);
+    
+        data_line = find_line(data,ref);
+        ajouter(data_line);
+      }   
     });
-});			function find_line(data_in,val){
-				len = data_in.length;
-				for (i = 0; i < len; i++) {
-					if(data_in[i][0] == val){
-						return data[i];					
-					}
-				}
-			}
+    });
+});         
+    function find_line(data_in,val)
+    {
+        len = data_in.length;
+                for (i = 0; i < len; i++) {
+                    if(data_in[i][0] == val){
+                        return data[i];                 
+                    }
+                }
+    }
 
             function ajouter(data_in)
             {
+                var monPanier = new Panier();
                 var code = parseInt(data_in[0]);
                 var name = data_in[1];
                 var qte = parseInt(document.getElementById("qte").value);
                 var prix = parseFloat(data_in[2]);
-                var monPanier = new Panier();
                 monPanier.ajouterArticle(code,name, qte, prix);
                 var tableau = document.getElementById("tableau");
                 var longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
@@ -100,15 +104,16 @@ $.ajax({
                 {
                     for(var i = longueurTab ; i > 0  ; i--)
                     {
-                        monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), tableau.rows[i].cells[1].innerHTML,parseInt(tableau.rows[i].cells[2].innerHTML), parseInt(tableau.rows[i].cells[3].innerHTML));
+                        monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), tableau.rows[i].cells[1].innerHTML,parseInt(tableau.rows[i].cells[2].innerHTML), parseFloat(tableau.rows[i].cells[3].innerHTML),parseFloat(tableau.rows[i].cells[4].innerHTML));
                         tableau.deleteRow(i);
                     }
                 }
+
                 var longueur = monPanier.liste.length;
                 for(var i = 0 ; i < longueur ; i++)
                 {
                     var ligne = monPanier.liste[i];
-                    console.log(ligne);
+                    console.log(ligne); 
                     var ligneTableau = tableau.insertRow(-1);
                     var colonne1 = ligneTableau.insertCell(0);
                     colonne1.innerHTML += ligne.getCode();
@@ -117,9 +122,11 @@ $.ajax({
                     var colonne3 = ligneTableau.insertCell(2);
                     colonne3.innerHTML += ligne.qteArticle;
                     var colonne4 = ligneTableau.insertCell(3);
-                    colonne4.innerHTML += ligne.getPrixLigne();
+                    colonne4.innerHTML += ligne.getPrix();
                     var colonne5 = ligneTableau.insertCell(4);
-                    colonne5.innerHTML += "<button class=\"btn btn-danger\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Remove</button>";
+                    colonne5.innerHTML += ligne.getPrixLigne();
+                    var colonne6 = ligneTableau.insertCell(5);
+                    colonne6.innerHTML += "<button class=\"btn btn-danger\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Remove</button>";
                 
                     }
                                     document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier();
@@ -135,27 +142,31 @@ $.ajax({
                 {
                     for(var i = longueurTab ; i > 0  ; i--)
                     {
-                        monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), parseInt(tableau.rows[i].cells[1].innerHTML), parseInt(tableau.rows[i].cells[2].innerHTML));
+                        monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), tableau.rows[i].cells[1].innerHTML,parseInt(tableau.rows[i].cells[2].innerHTML), parseFloat(tableau.rows[i].cells[3].innerHTML));
                         tableau.deleteRow(i);
                     }
                 }
                 monPanier.supprimerArticle(code);
                 var longueur = monPanier.liste.length;
-                for(var i = 0 ; i < longueur ; i++)
+                 for(var i = 0 ; i < longueur ; i++)
                 {
                     var ligne = monPanier.liste[i];
+                    console.log(ligne); 
                     var ligneTableau = tableau.insertRow(-1);
                     var colonne1 = ligneTableau.insertCell(0);
                     colonne1.innerHTML += ligne.getCode();
                     var colonne2 = ligneTableau.insertCell(1);
-                    colonne2.innerHTML += ligne.qteArticle;
+                    colonne2.innerHTML += ligne.getName();
                     var colonne3 = ligneTableau.insertCell(2);
-                    colonne3.innerHTML += ligne.prixArticle;
+                    colonne3.innerHTML += ligne.qteArticle;
                     var colonne4 = ligneTableau.insertCell(3);
-                    colonne4.innerHTML += ligne.getPrixLigne();
+                    colonne4.innerHTML += ligne.getPrix();
                     var colonne5 = ligneTableau.insertCell(4);
-                    colonne5.innerHTML += "<button class=\"btn btn-danger\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Remove</button>";
-                }
+                    colonne5.innerHTML += ligne.getPrixLigne();
+                    var colonne6 = ligneTableau.insertCell(5);
+                    colonne6.innerHTML += "<button class=\"btn btn-danger\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Remove</button>";
+                
+                    }
                 document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier();
                 document.getElementById("nbreLignes").innerHTML = longueur;
             }
@@ -186,13 +197,13 @@ $.ajax({
 
 <!-- Page content -->
 <div class="w3-content" style="max-width:1100px">
-	<h1 class="w3-center w3-xxxlarge">Café Michel</h1>
+    <h1 class="w3-center w3-xxxlarge">Café Michel</h1>
   <!-- About Section -->
   <div class="w3-row w3-padding-64" id="about">
 
 
       <h1 class="w3-center">Les tarifs</h1>
-	<div class="w3-center zoom l4 w3-padding-large">
+    <div class="w3-center zoom l4 w3-padding-large">
 <img src="tarif-CM-2018-1.jpg" class="w3-round w3-image w3-opacity-min fancybox" alt="Menu" style="width:100%">
 <img src="tarif-CM-2018-2.jpg" class="w3-round w3-image w3-opacity-min fancybox" alt="Menu" style="width:100%">
 
@@ -202,15 +213,13 @@ $.ajax({
   <hr>
   
   <!-- Menu Section -->
-  
-
   <!-- Menu Section -->
   <div class="w3-row w3-padding-64" id="menu">
         <h1 class="w3-center">Orders</h1><br>
   <div class="w3-col l6 orders">
-		<h3 class="w3-center">Add an article</h3><br>
+        <h3 class="w3-center">Add an article</h3><br>
     <section  class="container">
-            <article class="well form-inline pull-left col-lg-5">
+            <article class=" form-inline pull-left col-lg-5">
                 
                 <p><label>Reference</label> <input class="w3-input w3-padding-16" type="text" id = "id" ></p>
       <p><label>Quantity</label><input class="w3-input w3-padding-16" type="number" id = "qte" required name="Quantité"></p>
@@ -224,7 +233,7 @@ $.ajax({
     <div class="w3-col l6">
     <h3 class="w3-center">Shopping cart</h3><br><br>
              <section class="container">
-            <article class="well form-inline pull-left col-lg-5">
+            <article class=" form-inline pull-left col-lg-5">
                 <table id="tableau" class="table table-hover table-bordered">
                     <thead>
                         <tr>
@@ -232,6 +241,7 @@ $.ajax({
                             <th>Name</th>
                             <th>Quantity</th>
                             <th>Cost</th>
+                            <th>Total</th>
                             <th></th>
                         </tr>
                     </thead>
